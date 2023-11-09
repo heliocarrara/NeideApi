734dca85775943ce21dcab7c9576205aaa554858
heliocarrara <72034770+heliocarrara@users.noreply.github.com>
2023-11-09 18:26:58 +0000

    Mudança de url para host na nuvem

--- main.py
+++ main.py
@@ -5,22 +5,21 @@ app = Flask(__name__)
 
 # Construir funcionalidades
 
+
 @app.route('/')
 def homepage():
   return 'A Api está no ar!'
 
 
-@app.route('/pegarvendar')
+@app.route('/pegarvendas')
 def pegarvendas():
   tabela = pd.read_csv('12-18 - Criando API no Python.csv')
   total_vendas = tabela['Vendas'].sum()
 
-  resposta = { 'Total vendas:': total_vendas}
-  
+  resposta = {'Total vendas:': total_vendas}
+
   return jsonify(resposta)
 
 
 # Rodar
-app.run(host='0.0.0.0', port=8080)
-
-
+app.run(host='https://rnaneide.azurewebsites.net/', port=8080)
